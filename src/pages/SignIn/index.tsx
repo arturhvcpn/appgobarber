36d4b85f1d587fb22f94e5034cfe164d422b4cbe
Artur <arturcastropolonorte@gmail.com>
2020-08-22 10:21:26 -0300

    :sparkles: create structure and pages SignIn and SignUp

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image,View,ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -9,6 +10,8 @@ import logoImg from '../../assets/logo.png';
 import { Container, Title, ForgotPassword, ForgotPasswordText,CreateAccountButton,CreateAccountText } from './styles';
 
 const SignIn: React.FC = () =>{
+  const navigation = useNavigation();
+
   return(
   <>
   <KeyboardAvoidingView
@@ -39,7 +42,7 @@ const SignIn: React.FC = () =>{
         </ScrollView>
     </KeyboardAvoidingView>
 
-    <CreateAccountButton onPress ={() =>{}}>
+    <CreateAccountButton onPress ={() =>{ navigation.navigate('SignUp')}}>
       <Icon name="log-in" size={20} color="#ff9000" />
       <CreateAccountText> Criar uma conta</CreateAccountText>
     </CreateAccountButton>
